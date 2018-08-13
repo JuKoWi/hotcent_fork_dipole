ac96e230ac7b17dcfe5e6988ae17339e80336542
mvdb <maxime.cp.vandenbossche@gmail.com>
2018-08-14 00:45:04 +0200

    ENH: created Confinement classes

---
+++ hotcent/confinement.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# TODO: units!
+
+class PowerConfinement():
+    def __init__(self, r0=1., s=2):
+        self.r0 = r0
+        self.s = s
+
+    def __call__(self, r):
+        return (self.r0 / r) ** self.s
+
+
+class WoodsSaxonConfinement():
+    def __init__(self, v0=1., r0=1., a=1.): 
+        self.v0 = v0
+        self.r0 = r0
+        self.a = a
+
+    def __call__(self, r):
+        return -1 * self.v0 / (1 + np.exp((r - self.r0) / self.a))
