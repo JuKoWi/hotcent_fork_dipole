e4b26e59cb19ca5ca27ae88780293d18ed5fb9a2
mvdb <maxime.cp.vandenbossche@gmail.com>
2018-10-14 00:49:08 +0000

    BUG: corrected WoodsSaxon Vconf expr

--- hotcent/confinement.py
+++ hotcent/confinement.py
@@ -1,6 +1,7 @@
 """ Definition of confinement potentials """
 import numpy as np
 
+
 class PowerConfinement():
     def __init__(self, r0=1., s=2):
         self.r0 = r0
@@ -11,10 +12,10 @@ class PowerConfinement():
 
 
 class WoodsSaxonConfinement():
-    def __init__(self, v0=1., r0=1., a=1.): 
-        self.v0 = v0
+    def __init__(self, w=1., r0=1., a=1.):
+        self.w = w
         self.r0 = r0
         self.a = a
 
     def __call__(self, r):
-        return -1 * self.v0 / (1 + np.exp((r - self.r0) / self.a))
+        return self.w / (1 + np.exp(self.a * (self.r0 - r)))
