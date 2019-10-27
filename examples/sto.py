f09790d6918270b66aa7c227212338178be225d8
mvdb <maxime.cp.vandenbossche@gmail.com>
2019-10-27 04:23:46 -0400

    XMP: also plot numerical orbitals

--- examples/sto.py
+++ examples/sto.py
@@ -19,6 +19,7 @@ atom = AE('Sn',
 
 atom.run()
 atom.plot_density(filename='Sn_densities.png')
+atom.plot_Rnl(filename='Sn_orbitals.png')
 atom.fit_sto('5s', 5, 4, filename='Sn_5s_STO.png')
 atom.fit_sto('5p', 5, 4, filename='Sn_5p_STO.png')
 atom.fit_sto('4d', 5, 4, filename='Sn_4d_STO.png')
