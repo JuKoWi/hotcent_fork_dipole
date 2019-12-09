2e04ec31ae50b715c7d99f63b1c4a4319d5cd900
mvdb <maxime.cp.vandenbossche@gmail.com>
2019-12-08 20:28:16 -0500

    TST: less-tight tolerance on integrals

--- examples/test.py
+++ examples/test.py
@@ -2,7 +2,8 @@ from hotcent.slako import SlaterKosterTable
 from hotcent.confinement import PowerConfinement
 from hotcent.atomic_dft import AtomicDFT
 
-eps = 1e-7
+eps_eig = 1e-7
+eps_sk = 5e-7
 eps_etot = 1e-7
 
 def check(x, y, eps):
@@ -26,9 +27,9 @@ atom1.run()
 ener = atom1.get_energy()
 print('B -- Etot  | %s' % check(ener, -23.079723850586106, eps_etot))
 e_2s = atom1.get_epsilon('2s')
-print('B -- E_2s  | %s' % check(e_2s, 0.24990807910273996, eps))
+print('B -- E_2s  | %s' % check(e_2s, 0.24990807910273996, eps_eig))
 e_2p = atom1.get_epsilon('2p')
-print('B -- E_2p  | %s' % check(e_2p, 0.47362603289831301, eps))
+print('B -- E_2p  | %s' % check(e_2p, 0.47362603289831301, eps_eig))
 
 
 # Check confined hydrogen atom
@@ -43,7 +44,7 @@ atom2.run()
 ener = atom2.get_energy()
 print('H -- Etot  | %s' % check(ener, 0.58885808402033557, eps_etot))
 e_1s = atom2.get_epsilon('1s')
-print('H -- E_1s  | %s' % check(e_1s, 1.00949638195278960, eps))
+print('H -- E_1s  | %s' % check(e_1s, 1.00949638195278960, eps_eig))
 
 
 # Check B-H Slater-Koster integrals
@@ -56,13 +57,13 @@ grid, areas = sk.make_grid(R, nt=nt, nr=nr)
 S, H, H2 = sk.calculate_mels([('sss', '2s', '1s')], atom1, atom2,
                              R, grid, areas)
 
-print('B-H sps S  | %s' % check_abs(S[9], -0.34627316, eps))
-print('B-H sps H  | %s' % check_abs(H[9], 0.29069894, eps))
-print('B-H sps H2 | %s' % check_abs(H2[9], 0.29077536, eps))
+print('B-H sps S  | %s' % check_abs(S[9], -0.34627316, eps_sk))
+print('B-H sps H  | %s' % check_abs(H[9], 0.29069894, eps_sk))
+print('B-H sps H2 | %s' % check_abs(H2[9], 0.29077536, eps_sk))
 
 S, H, H2 = sk.calculate_mels([('sps', '1s', '2p')], atom2, atom1, 
                              R, grid, areas)
 
-print('H-B sps S  | %s' % check_abs(S[8], -0.47147340, eps))
-print('H-B sps H  | %s' % check_abs(H[8], 0.33033262, eps))
-print('H-B sps H2 | %s' % check_abs(H2[8], 0.33028882, eps))
+print('H-B sps S  | %s' % check_abs(S[8], -0.47147340, eps_sk))
+print('H-B sps H  | %s' % check_abs(H[8], 0.33033262, eps_sk))
+print('H-B sps H2 | %s' % check_abs(H2[8], 0.33028882, eps_sk))
