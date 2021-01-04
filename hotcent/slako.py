41733c3b3266c19d7298ad165517374157975dd0
mvdb <maxime.cp.vandenbossche@gmail.com>
2021-01-04 16:10:38 -0500

    LIC: add SPDX license headers

--- hotcent/slako.py
+++ hotcent/slako.py
@@ -1,3 +1,9 @@
+#-----------------------------------------------------------------------------#
+#   Hotcent: calculating one- and two-center Slater-Koster integrals,         #
+#            based on parts of the Hotbit code                                #
+#   Copyright 2018-2021 Maxime Van den Bossche                                #
+#   SPDX-License-Identifier: GPL-3.0-or-later                                 #
+#-----------------------------------------------------------------------------#
 """ Defintion of the SlaterKosterTable class (and
 supporting methods) for calculating Slater-Koster
 integrals.
