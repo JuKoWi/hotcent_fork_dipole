997b191fd4b6c98c8591609fa392195a77d8a1e2
mvdb <maxime.cp.vandenbossche@gmail.com>
2021-09-12 15:17:47 -0400

    DEV: add SPDX headers to new files

--- hotcent/offsite_threecenter.py
+++ hotcent/offsite_threecenter.py
@@ -1,3 +1,9 @@
+#-----------------------------------------------------------------------------#
+#   Hotcent: calculating one- and two-center Slater-Koster integrals,         #
+#            based on parts of the Hotbit code                                #
+#   Copyright 2018-2021 Maxime Van den Bossche                                #
+#   SPDX-License-Identifier: GPL-3.0-or-later                                 #
+#-----------------------------------------------------------------------------#
 import numpy as np
 from scipy.integrate import quad_vec
 from scipy.interpolate import SmoothBivariateSpline
