b4bd506d1c5e58b1d65c76f139fef0170a7d6c71
mvdb <maxime.cp.vandenbossche@gmail.com>
2022-12-22 18:52:29 +0100

    ENH: add solid harmonics module

---
+++ hotcent/solid_harmonics.py
@@ -0,0 +1,54 @@
+#-----------------------------------------------------------------------------#
+#   Hotcent: calculating one- and two-center Slater-Koster integrals,         #
+#            based on parts of the Hotbit code                                #
+#   Copyright 2018-2021 Maxime Van den Bossche                                #
+#   SPDX-License-Identifier: GPL-3.0-or-later                                 #
+#-----------------------------------------------------------------------------#
+import numpy as np
+from hotcent.orbitals import ANGULAR_MOMENTUM
+from hotcent.spherical_harmonics import sph_cartesian
+
+
+def sph_solid_radial(r, l):
+    """
+    Returns the radial part of the selected (regular) solid harmonic.
+
+    Parameters
+    ----------
+    r : float or np.ndarray
+        Corresponding distances from the origin.
+    l : int
+        Angular momentum.
+
+    Returns
+    -------
+    R : float or np.ndarray
+        Function value(s).
+    """
+    R = np.sqrt(4 * np.pi / (2*l + 1)) * r**l
+    return R
+
+
+def sph_solid(x, y, z, r, lm):
+    """
+    Returns the value of the selected (regular) solid harmonic.
+
+    Parameters
+    ----------
+    x, y, z : float or np.ndarray
+        Cartesian coordinates.
+    r : float or np.ndarray
+        Corresponding distances from the origin.
+    lm : str
+        Orbital label (e.g. 'px').
+
+    Returns
+    -------
+    C : float or np.ndarray
+        Function value(s).
+    """
+    l = ANGULAR_MOMENTUM[lm[0]]
+    R = sph_solid_radial(r, l)
+    Y = sph_cartesian(x, y, z, r, lm)
+    C = R * Y
+    return C
