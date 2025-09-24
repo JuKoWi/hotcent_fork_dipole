9e9cbc87bedf22bf9049dce7457008d9d031bdb1
JuKoWi <julius.willrich@uni-jena.de>
2025-09-24 16:13:37 +0200

    added s and p dipole integrals

--- hotcent/offsite_twocenter_dipole.py
+++ hotcent/offsite_twocenter_dipole.py
@@ -1,11 +1,14 @@
 import numpy as np
 from hotcent.multiatom_integrator import MultiAtomIntegrator
+from hotcent.orbitals import ANGULAR_MOMENTUM
+from slako_dipole import (INTEGRALS, print_integral_overview, select_integrals, NUMSK, phi3)
+import matplotlib.pyplot as plt
 
 class Offsite2cTableDipole(MultiAtomIntegrator):
     def __init__(self, *args, **kwargs):
         MultiAtomIntegrator.__init__(self, *args, grid_type='bipolar', **kwargs)
 
-    def run(self, rmin=0.4, dr=0.02, N=None, superposition='density', xc='LDA', stride=1):
+    def run(self, rmin=0.4, dr=0.02, N=None, superposition='density', xc='LDA', nr=50, stride=1, wflimit=1e-7, ntheta=150):
 
         self.print_header()
 
@@ -14,4 +17,76 @@ class Offsite2cTableDipole(MultiAtomIntegrator):
         assert np.isclose(rmin / dr, np.round(rmin / dr)), \
                'rmin must be a multiple of dr'
         assert superposition in ['density', 'potential']
-        print('finished') 
+
+        self.timer.start('run_offsite2c') # TODO check what this does
+        wf_range = self.get_range(wflimit)
+        Nsub = N // stride
+        Rgrid = rmin + stride * dr * np.arange(Nsub)
+        tables = {}
+
+        for p, (e1, e2) in enumerate(self.pairs): # iterate over ordered element pairs
+            selected = select_integrals(e1, e2)
+            print_integral_overview(e1, e2, selected, file=self.txt)
+
+            for bas1 in range(len(e1.basis_sets)):
+                for bas2 in range(len(e2.basis_sets)):
+                    tables[(p, bas1, bas2)] = np.zeros((Nsub, 2*NUMSK))
+
+        for i, R in enumerate(Rgrid):
+            if R > 2 * wf_range:
+                break
+
+            grid, area = self.make_grid(R, wf_range, nt=ntheta, nr=nr)
+
+            if i == Nsub - 1 or Nsub // 10 == 0 or i % (Nsub // 10) == 0:
+                print('R=%8.2f, %i grid points ...' % (R, len(grid)),
+                      file=self.txt, flush=True)
+
+            for p, (e1, e2) in enumerate(self.pairs):
+                selected = select_integrals(e1, e2)
+                if len(grid) > 0:
+                    D = self.calculate(selected, e1, e2, R, grid, area,
+                                             xc=xc, superposition=superposition)
+                    for key in selected:
+                        integral, nl1, nl2 = key
+                        bas1 = e1.get_basis_set_index(nl1)
+                        bas2 = e2.get_basis_set_index(nl2)
+                        index = INTEGRALS.index(integral)
+                        tables[(p, bas1, bas2)][i, index] = H[key]
+                        tables[(p, bas1, bas2)][i, NUMSK+index] = S[key]
+        #TODO: do I have to worry about pseudopotentials, probably not
+
+        self.timer.stop('run_offsite2c')
+
+    def calculate(self, selected, e1, e2, R, grid, area):
+
+        self.timer.start('calculate_offsite2c')
+
+        # common for all integrals (not wf-dependent parts)
+        self.timer.start('prelude')
+        x = grid[:, 0]
+        y = grid[:, 1]
+        r1 = np.sqrt(x**2 + y**2)
+        r2 = np.sqrt(x**2 + (y - R)**2)
+        c1 = y / r1  # cosine of theta_1
+        c2 = (y - R) / r2  # cosine of theta_2
+        s1 = x / r1  # sine of theta_1
+        s2 = x / r2  # sine of theta_2
+
+        # calculate all selected integrals
+        Dl= {}
+        sym1, sym2 = e1.get_symbol(), e2.get_symbol()
+
+        for key in selected:
+            integral, nl1, nl2 = key
+
+            gphi = phi3(c1, c2, s1, s2, integral)
+            aux = gphi * area * x * r1
+
+            Rnl1 = e1.Rnl(r1, nl1)
+            Rnl2 = e2.Rnl(r2, nl2)
+            D = np.sum(Rnl1 * Rnl2 * aux)
+            Dl[key] = D
+
+        self.timer.stop('calculate_offsite2c')
+        return Dl
--- hotcent/slako_dipole.py
+++ hotcent/slako_dipole.py
@@ -1,19 +1,93 @@
 import numpy as np
 
 """nonvanishing SlaKo integrals for dipole elements named in the form
-Y-p_n-Y-symmetry
+Y1*Y1*Y2
 """
-INTEGRALS = ['ppps', 'ppss', 'pppp', 'spss', 'spps'] 
+INTEGRALS = ['pzpzpz', 'pzpzss', 'sspzss', 'sspzpz', 'sspxpx', 'pxpxss', 'pxpxpz', 'pxpzpx' ] 
 
-def phi3(c1, c2, s1, s2, integral):
-    """ Returns the angle-dependent part of the given two-center integral,
+NUMSK = len(INTEGRALS)
+
+def phi3(c1, c2, s1, s2, sk_label): #TODO write integrals including d
+    """ Returns the angle-dependent part of the given two-center dipole-integral,
     with c1 and s1 (c2 and s2) the sine and cosine of theta_1 (theta_2)
     for the atom at origin (atom at z=Rz). These expressions are obtained
     by integrating analytically over phi.
     """
-    if integral == 'ppps':
+    if sk_label == 'pzpzpz':
         return 3/4 * np.sqrt(3/np.pi) * c1**2 * c2
-    elif integral == 'ppss':
+    elif sk_label == 'pzpzss':
         return 3/( 4 * np.sqrt(np.pi)) * c1**2
+    elif sk_label == 'sspzss':
+        return 1/4 * np.sqrt(3/np.pi) * c1
+    elif sk_label == 'sspzpz':
+        return 3/(4 * np.sqrt(np.pi)) * c1 * c2
+    elif sk_label == 'sspxpx':
+        return 3/(8 * np.sqrt(np.pi)) * s1 * s2
+    elif sk_label == 'pxpxss':
+        return 3/(8 * np.sqrt(np.pi)) * s1**2 
+    elif sk_label == 'pxpxpz':
+        return 3/8 * np.sqrt(3/np.pi) * s1**2 * c2
+    elif sk_label == 'pxpzpx': 
+        return 3/8 * np.sqrt(3/np.pi) * s1 * c1 * s2 
+
+def select_integrals(e1, e2):
+    """ Return list of integrals (integral, nl1, nl2)
+    to be done for element pair e1, e2. """
+    selected = []
+    for ival1, valence1 in enumerate(e1.basis_sets):
+        for ival2, valence2 in enumerate(e2.basis_sets):
+            for sk_label in INTEGRALS:
+                nl1, nl2 = select_subshells(valence1, valence2, sk_label)
+                if nl1 is not None and nl2 is not None:
+                    selected.append((sk_label, nl1, nl2))
+    return selected
+
+def select_subshells(val1, val2, sk_label):
+    """
+    Select subshells from given valence sets to calculate given
+    Slater-Koster integral.
+
+    Parameters
+    ----------
+    val1, val2 : list of str
+        Valence subshell sets (e.g. ['2s', '2p'], ['4s', '3d']).
+    integral : str
+        Slater-Koster integral label (e.g. 'pzpzpz').
+
+    Returns
+    -------
+    nl1, nl2 : str
+        Matching subshell pair (e.g. ('2s', '3d') in this example).
+    """
+    nl1 = None
+    for nl in val1:
+        if nl[1] == sk_label[0]:
+            nl1 = nl
+
+    nl2 = None
+    for nl in val2:
+        if nl[1] == sk_label[4]:
+            nl2 = nl
+
+    return nl1, nl2
+    
+    
+def print_integral_overview(e1, e2, selected, file):
+    """ Prints an overview of the selected Slater-Koster integrals. """
+    for bas1 in range(len(e1.basis_sets)):
+        for bas2 in range(len(e2.basis_sets)):
+            sym1 = e1.get_symbol() + '+'*bas1
+            sym2 = e2.get_symbol() + '+'*bas2
+            print('Integrals for %s-%s pair:' % (sym1, sym2), end=' ',
+                  file=file)
+            for integral, nl1, nl2 in selected:
+                if e1.get_basis_set_index(nl1) == bas1 and \
+                   e2.get_basis_set_index(nl2) == bas2:
+                    print('_'.join([nl1, nl2, integral]), end=' ', file=file)
+            print(file=file, flush=True)
+    return
+
+    
+
     
     
