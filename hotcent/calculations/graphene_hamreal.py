oversqr.dat almost works, one sign wrong

hotcent/calculations/graphene_hamreal.py
@@ -3,40 +3,65 @@ from ase import Atoms
 from ase.build import graphene
 from ase.neighborlist import *
 from ase.visualize import view
+from hotcent.new_dipole.assemble_integrals import SK_Integral
+from hotcent.new_dipole.utils import *
 
 
-def write_overreal(structure, orbdict):
+def write_overreal(structure, maxl_dict, skdir):
     idx1, idx2 = neighbor_list(quantities='ij', a=structure, self_interaction=True, cutoff=10)
     atomtypes =structure.get_chemical_symbols()
-    orbnumbers = [orbdict[key] for key in atomtypes]
+    orbnumbers = [get_norbs(maxl_dict[key]) for key in atomtypes]
     total_orbs = np.sum(orbnumbers)
     table = np.zeros((total_orbs, total_orbs))
-    for i, firstatom in enumerate(idx1):
-        for j, firstatom in enumerate(idx2):
-            pass
+    block_starts = []
 
-def find_central_equivaltens(atoms, tol=1e-12):
-    frac = atoms.get_scaled_positions()
-    wrapped = frac % 1
-    central_index = []
-    for i in range(len(atoms)):
-        target = wrapped[i]
-        diff = np.abs(wrapped-target)
-        diff = np.min(diff, 1-diff)
-        j = np.where(np.all(diff < tol, axis=1))[0][0]
-        central_index.append(j)
-    return central_index
+    count = 0
+    for type in structure.get_chemical_symbols():
+        block_starts.append(count)
+        count += get_norbs(maxl_dict[type])
+
+    for i, firstatom in enumerate(idx1): # iteration over pairs, not first atom
+        secondatom = idx2[i]
+        typeA = structure.symbols[firstatom]
+        typeB = structure.symbols[secondatom]
+        posA = structure.positions[firstatom]
+        posB = structure.positions[secondatom]
+        max_lA = maxl_dict[typeA]
+        max_lB = maxl_dict[typeB]
+        pair = Atoms(f'{typeA}{typeB}', positions=[posA, posB])
+        skpath = skdir + f'/{typeA}-{typeB}.skf'
         
+        pair_integral = SK_Integral()
+        pair_integral.load_atom_pair(atoms=pair)
+        pair_integral.load_sk_file(path=skpath)
+        S_vec = pair_integral.calculate(hamilton=False)
+
+        start_row = block_starts[firstatom]
+        stop_row = start_row + get_norbs(max_lA)
+        start_col = block_starts[secondatom]
+        stop_col = start_col + get_norbs(max_lB) 
+        if secondatom == firstatom:
+            assert typeA == typeB
+            table[start_row:stop_row, start_col:stop_col] = np.eye(get_norbs(max_lA))
+        else:
+            table[start_row:stop_row, start_col:stop_col] = pair_integral.select_overlap_elements(max_lA=max_lA, max_lB=max_lB)
     
+    header1 = f"#\tREAL\tNALLORB\tNKPOINT\n"
+    header2 = f"\tT\t{total_orbs}\t1\n" 
+    header3 = "#IKPOINT\n"
+    header4 = "\t1\n"
+    header5 = "#MATRIX"
+    header = header1 + header2 + header3 + header4 + header5
+    np.savetxt(fname='oversqr.dat', X=table, header=header, comments='')
 
 
-g = graphene(size=(2,2,1), vacuum=10.0)
-orbdict = {'C':4}
-g.pbc = [False, False, False]
+# g = graphene(size=(2,2,1), vacuum=10.0)
+max_l_dict = {'C':1}
+# g.pbc = [False, False, False]
 # cutoffs = natural_cutoffs(structure)
 # print(neighbor_list(quantities='ijD', a=g, cutoff=3))
-atoms = Atoms('C2', positions=[[0,0,0], [1,0,0]])
-write_overreal(structure=atoms, orbdict=orbdict)
+atoms = Atoms('C2', positions=[[0,0,0], [0,0,1]])
+write_overreal(structure=atoms, maxl_dict=max_l_dict, skdir="./skfiles")
 
 
 

hotcent/new_dipole/assemble_integrals.py
@@ -3,6 +3,7 @@ import ase as ase
 import pickle
 import os
 import sys
+from pathlib import Path
 import sympy as sp
 from scipy.interpolate import CubicSpline
 from hotcent.new_dipole.utils import *
@@ -59,7 +60,7 @@ class SK_Integral:
         self.quant_nums_dipole = quant_num_list
         self.sk_int_idx_dipole = nonzeros
 
-    def load_atom_pair(self, path):
+    def load_atom_file(self, path):
         """load position and basis functions to compute the integrals for"""
         atoms = ase.io.read(path)
         self.R_vec = angstrom_to_bohr(atoms.get_distance(0, 1, vector=True)) 
@@ -67,6 +68,15 @@ class SK_Integral:
         atom1 = angstrom_to_bohr(atoms.get_positions()[0])
         self.atom1_pos = np.array([atom1[1], atom1[2], atom1[0]]) # order consistent with quantum numbers
         self._set_euler_angles()
+    
+    def load_atom_pair(self, atoms):
+        """load a single pair as an ase.atoms object"""
+        self.R_vec = angstrom_to_bohr(atoms.get_distance(0, 1, vector=True)) 
+        self.R = angstrom_to_bohr(atoms.get_distance(0,1, vector=False))
+        atom1 = angstrom_to_bohr(atoms.get_positions()[0])
+        self.atom1_pos = np.array([atom1[1], atom1[2], atom1[0]]) # order consistent with quantum numbers
+        self._set_euler_angles()
+        
 
     def _set_euler_angles(self):
         """use only two rotations of three possible"""
@@ -82,15 +92,18 @@ class SK_Integral:
 
     def load_sk_file(self, path):
         """.skf file for H and S"""
+        myfile = Path(path)
+        assert myfile.is_file()
         with open(path, "r") as f:
+            print('opened file')
             first_line = f.readline().strip()
             extended = 1 if first_line.startswith('@') else 0
-            for i,line in enumerate(f, start=1):
-                if i - extended == 0:
-                    line1 = line.strip()
-                    parts = [p.strip() for p in line1.split(', ')]
-                    delta_R, n_points = float(parts[0]), int(parts[1])
-                    break
+            if extended == 0:
+                parts = [p.strip() for p in first_line.split(', ')]
+            if extended == 1:
+                line2 = f.readline().strip()
+                parts = [p.strip() for p in line2.split(',')]
+            delta_R, n_points = float(parts[0]), int(parts[1])
         data = np.loadtxt(path, skiprows=3+extended)
         self.delta_R = delta_R
         self.n_points = n_points 
@@ -143,24 +156,61 @@ class SK_Integral:
         integral_vec = np.zeros((len(self.quant_nums)))
         for i, key in enumerate(sorted(INTEGRALS, key= lambda x: x[0])):
             integral_vec[key[0]] = cs(self.R)[i]
-        dipole_elements = self.D_full @ integral_vec
-        return dipole_elements
+        integrals = self.D_full @ integral_vec
+        if hamilton:
+            self.H_vec = integrals
+            H_dict = {}
+            for label in self.quant_nums:
+                H_dict[(label[1], label[2, label[3], label[4]])] = integrals[label[0]]
+            self.H_dict = H_dict
+        else:
+            self.S_vec = integrals
+            S_dict = {}
+            for label in self.quant_nums:
+                S_dict[(label[1],label[2],label[3],label[4])] = integrals[label[0]]
+            self.S_dict = S_dict
+        return integrals
 
     def calculate_dipole(self):
         self._set_rotation_matrix_dipole()
         overlap_elements = self.calculate(hamilton=False)
-
+        self.S_vec = overlap_elements
         R_grid_dipole = self.delta_R_dipole + self.delta_R_dipole * np.arange(self.n_points_dipole) 
         cs_dipole = CubicSpline(R_grid_dipole, self.sk_table_dipole) 
         integral_vec_dipole = np.zeros((len(self.quant_nums_dipole)))
         for i, key in enumerate(sorted(INTEGRALS_DIPOLE, key= lambda x: x[0])):
             integral_vec_dipole[key[0]] = cs_dipole(self.R)[i]
         dipole_elements = self.D_full_dipole @ integral_vec_dipole
-        print(self.R)
 
         overlap_blocks = overlap_elements.reshape(16,16)
         shift_term = np.tile(overlap_blocks, (1,3)).reshape(-1)
         space_factor = np.tile(np.repeat(self.atom1_pos, 16), 16)
         shift_term = shift_term * space_factor
         shifted_dipole = dipole_elements + shift_term
+        self.d_vec = shifted_dipole
         return shifted_dipole
+    
+    def select_overlap_elements(self, max_lA, max_lB):
+        """returns 2D array of matrix elements"""
+        pair_overlap_matrix = np.zeros((get_norbs(max_lA), get_norbs(max_lB)))        
+        row_start = 0
+        col_start = 0
+        for l1 in range(max_lA + 1):
+            size_row = 2 * l1 +1
+            for l2 in range(max_lB + 1):
+                size_col = 2 * l2 +1
+                block = np.zeros((size_row, size_col))
+                for mi, m in enumerate(range(-l1, l1 + 1)):
+                    for ni, n in enumerate(range(-l2, l2 + 1)):
+                        block[mi, ni] = self.S_dict[(l1, m, l2, n)]
+                pair_overlap_matrix[row_start:row_start+size_row, col_start:col_start+size_col] = block
+                col_start += size_col
+            col_start = 0
+            row_start += size_row
+        return pair_overlap_matrix
+        
+
+
+
+
+        

hotcent/new_dipole/offsite_twocenter_new.py
@@ -61,7 +61,6 @@ class Offsite2cTable(MultiAtomIntegrator):
             for p, (e1, e2) in enumerate(self.pairs):
                 selected = select_integrals(e1, e2)
                 label_list = sorted(INTEGRALS.keys(), key=lambda x: x[0])
-                print(label_list)
                 if len(grid) > 0:
                     S, H, H2 = self.calculate(selected, e1, e2, R, grid, area,
                                              xc=xc, superposition=superposition, zeta=zeta)
@@ -218,7 +217,7 @@ class Offsite2cTable(MultiAtomIntegrator):
 
     def write(self, eigenvalues=None, dftbplus_format=False, hubbardvalues=None, occupations=None,
               spe=None, offdiagonal_H=None, offdiagonal_S=None,
-              filename_template='{el1}-{el2}_offsite2c.skf'):
+              filename_template='{el1}-{el2}.skf'):
         """
         Writes all Slater-Koster integral tables to file.
         By default the 'simple' SKF format is chosen, and the 'extended'
@@ -304,11 +303,8 @@ class Offsite2cTable(MultiAtomIntegrator):
                     table = self.tables[(p, bas1, bas2)]
                     if dftbplus_format:
                         table1 = convert_table_dftbplus(table[:,:NUMSK])
-                        print(np.shape(table1))
                         table2 = convert_table_dftbplus(table[:,NUMSK:])
-                        print(np.shape(table2))
                         table = np.concatenate((table1, table2), axis=1)
-                    print(np.shape(table))
                     with open(filename, 'w') as f:
                         write_skf(f, self.Rgrid, table, has_diagonal_data,
                                   is_extended, eigval, hubval, occup, SPE, mass,